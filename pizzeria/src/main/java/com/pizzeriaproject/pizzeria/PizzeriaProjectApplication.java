6779274931b4cbc54628717fd208b9e0c722a0c1
ostapula <ostapnote2@gmail.com>
2023-10-14 00:17:18 +0200

    Initialized the spring boot project.

---
+++ pizzeria/src/main/java/com/pizzeriaproject/pizzeria/PizzeriaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.pizzeriaproject.pizzeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzeriaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzeriaProjectApplication.class, args);
+	}
+
+}
